65f8ccff2f98c5948dcf491b6ce83221dd7e88c3
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-08-27 01:10:29 +0530

    message

---
+++ App.js
@@ -0,0 +1,9 @@
+const element = React.createElement(
+    'h1',
+    { style: { color: 'blue' } },
+    'Hello, React.createElement!'
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(element);

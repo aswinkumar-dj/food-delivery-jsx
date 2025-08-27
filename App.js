f97cb30aceb8e7f66d0c1177214ca190548469c5
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-08-27 19:02:11 +0530

    update

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-const element = React.createElement(
-    'h1',
-    { style: { color: 'blue' } },
-    'Hello, React.createElement!'
-);
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(element);

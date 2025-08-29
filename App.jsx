0b143fec122bcc04570e7ec6a124224774235aed
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-08-29 22:40:59 +0530

    styling done

--- App.jsx
+++ App.jsx
@@ -8,7 +8,8 @@ const Header = () => {
         <img
           className="logo"
           src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png"
-        alt="logo"/>
+          alt="logo"
+        />
       </div>
       <div className="nav-items">
         <ul>
@@ -22,10 +23,57 @@ const Header = () => {
   );
 };
 
+const RestaurantList = () => {
+  return (
+    <div className="restaurant-list">
+      <div className="restaurant-card">
+        <img
+          className="restaurant-logo"
+          src="https://tse3.mm.bing.net/th/id/OIP.ZoD3AyqhENWSedWwk-7QjwHaE8?pid=Api&P=0&h=180"
+          alt="restaurant-logo"
+        />
+        <h3>Restaurant Name</h3>
+        <h4>American-cusine</h4>
+        <h4>4.5 stars</h4>
+        <h4>30 mins</h4>
+      </div>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <input type="text" placeholder="Search for restaurants and food" />
+      <div className="restaurant-list-container">
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+        <RestaurantList />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };

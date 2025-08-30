ae78667fd768c4db1465d04ff2d2cc8283576003
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-08-30 21:49:20 +0530

    Understanding props

--- App.jsx
+++ App.jsx
@@ -23,19 +23,107 @@ const Header = () => {
   );
 };
 
-const RestaurantList = () => {
+const thirdImage =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Dominos_pizza_logo.svg/200px-Dominos_pizza_logo.svg.png";
+
+const ResList = [
+  {
+    id: 1,
+    name: "Burger King",
+    cuisine: "American",
+    rating: 4.2,
+    deliveryTime: "30 mins",
+    image: thirdImage,
+  },
+  {
+    id: 2,
+    name: "Pizza Hut",
+    cuisine: "Italian",
+    rating: 4.0,
+    deliveryTime: "35 mins",
+    image: thirdImage,
+  },
+  {
+    id: 3,
+    name: "Domino's Pizza",
+    cuisine: "Italian",
+    rating: 4.1,
+    deliveryTime: "32 mins",
+    image: thirdImage,
+  },
+  {
+    id: 4,
+    name: "KFC",
+    cuisine: "American",
+    rating: 4.3,
+    deliveryTime: "28 mins",
+    image: thirdImage,
+  },
+  {
+    id: 5,
+    name: "Subway",
+    cuisine: "Healthy",
+    rating: 4.0,
+    deliveryTime: "25 mins",
+    image: thirdImage,
+  },
+  {
+    id: 6,
+    name: "McDonald's",
+    cuisine: "American",
+    rating: 4.2,
+    deliveryTime: "27 mins",
+    image: thirdImage,
+  },
+  {
+    id: 7,
+    name: "Biryani Blues",
+    cuisine: "Indian",
+    rating: 4.4,
+    deliveryTime: "40 mins",
+    image: thirdImage,
+  },
+  {
+    id: 8,
+    name: "Haldiram's",
+    cuisine: "Indian",
+    rating: 4.3,
+    deliveryTime: "33 mins",
+    image: thirdImage,
+  },
+  {
+    id: 9,
+    name: "Faasos",
+    cuisine: "Wraps & Rolls",
+    rating: 4.1,
+    deliveryTime: "29 mins",
+    image: thirdImage,
+  },
+  {
+    id: 10,
+    name: "Behrouz Biryani",
+    cuisine: "Biryani",
+    rating: 4.5,
+    deliveryTime: "38 mins",
+    image: thirdImage,
+  },
+];
+
+const RestaurantList = (props) => {
+  const { resData } = props;
+
   return (
     <div className="restaurant-list">
       <div className="restaurant-card">
         <img
           className="restaurant-logo"
-          src="https://tse3.mm.bing.net/th/id/OIP.ZoD3AyqhENWSedWwk-7QjwHaE8?pid=Api&P=0&h=180"
+          src={resData.image}
           alt="restaurant-logo"
         />
-        <h3>Restaurant Name</h3>
-        <h4>American-cusine</h4>
-        <h4>4.5 stars</h4>
-        <h4>30 mins</h4>
+        <h3>{resData.name}</h3>
+        <h4>{resData.cuisine}</h4>
+        <h4>{resData.rating} Star rating</h4>
+        <h4>{resData.deliveryTime}</h4>
       </div>
     </div>
   );
@@ -45,25 +133,9 @@ const Body = () => {
     <div className="body">
       <input type="text" placeholder="Search for restaurants and food" />
       <div className="restaurant-list-container">
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
-        <RestaurantList />
+        {ResList.map((restaurants) => {
+          return <RestaurantList key={restaurants.id} resData={restaurants} />;
+        })}
       </div>
     </div>
   );

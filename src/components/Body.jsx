9130b32d0bd32e3eeda5c8318ccbe1a19500a2eb
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-09-02 11:44:25 +0530

    top rated feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 import { ResList } from "../utils/mockData.js";
 import RestaurantList from "./RestaurantList.jsx";
 const Body = () => {
+  const [ListOfRestaurants, setListOfRestaurants] = useState(ResList);
   return (
     <div className="body">
-      <input type="text" placeholder="Search for restaurants and food" />
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ResList.filter((res) => res.rating > 4);
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className="restaurant-list-container">
-        {ResList.map((restaurants) => {
+        {ListOfRestaurants.map((restaurants) => {
           return <RestaurantList key={restaurants.id} resData={restaurants} />;
         })}
       </div>

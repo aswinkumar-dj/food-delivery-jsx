f5e1fdafbd0a2e3433b227f6dfb583276288bd22
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-09-13 08:39:50 +0530

    Github api integrated

--- src/components/AboutProfile.jsx
+++ src/components/AboutProfile.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-const AboutProfile = ({ name, status }) => {
+import React, { useState, useEffect } from "react";
+
+const AboutProfile = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      const response = await fetch(
+        "https://api.github.com/users/aswinkumar-dj"
+      );
+      const data = await response.json();
+      setUserData(data);
+    }
+
+    fetchUser();
+  }, []);
+
+  if (!userData) return <div className="profile-card"></div>;
+
   return (
-    <div>
-      <h1>Name:{name}</h1>
-      <p>status:{status}</p>
+    <div
+      className="profile-card"
+      onClick={() => {
+        window.open(userData.html_url, "_blank");
+      }}
+    >
+      <img
+        src={userData.avatar_url}
+        alt="Profile Avatar"
+        className="profile-avatar"
+      />
+      <h1 className="profile-name">{userData.name || userData.login}</h1>
+      <p className="profile-bio">{userData.bio || "No bio available"}</p>
     </div>
   );
 };
+
 export default AboutProfile;

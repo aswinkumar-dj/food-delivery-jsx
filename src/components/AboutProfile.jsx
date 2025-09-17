2c92e608dde906868709e7fa2e85748eb9c35134
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-09-18 02:31:28 +0530

    styling

--- src/components/AboutProfile.jsx
+++ src/components/AboutProfile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { GIT_ICON, LINKEDIN_ICON } from "../utils/constants";
 
 const AboutProfile = () => {
   const [userData, setUserData] = useState(null);
@@ -21,23 +22,39 @@ const AboutProfile = () => {
     );
 
   return (
-    <div
-      className="profile-card bg-white rounded-2xl shadow-lg hover:shadow-2xl hover: shadow-purple-400 transition transform hover:-translate-y-2 cursor-pointer p-8 max-w-md mx-auto flex flex-col items-center space-y-6 text-center mt-10"
-      onClick={() => {
-        window.open(userData.html_url, "_blank");
-      }}
-    >
+    <div className="profile-card bg-white rounded-2xl shadow-lg hover:shadow-2xl hover: shadow-purple-400 transition transform hover:-translate-y-2 cursor-pointer p-8 max-w-md mx-auto flex flex-col items-center space-y-6 text-center mt-10">
       <img
         src={userData.avatar_url}
         alt="Profile Avatar"
         className="profile-avatar w-36 h-36 rounded-full object-cover border-3 border-purple-700"
       />
-      <h1 className="profile-name text-2xl font-bold text-gray-900">
+      <h1 className="profile-name text-2xl font-bold text-gray-900 mb-2">
         {userData.name || userData.login}
       </h1>
-      <p className="profile-bio text-gray-700 text-base">
+      <p className="profile-bio text-gray-700 text-base ">
         {userData.bio || "No bio available"}
       </p>
+      <div className="social-icons flex space-x-6">
+        <img
+          src={LINKEDIN_ICON}
+          alt="LinkeIn Icon"
+          className="w-8 h-8 mt-0.5 flex hover:animate-pulse"
+          onClick={() => {
+            window.open(
+              "https://www.linkedin.com/in/aswinkumar-dj-5a50ba226",
+              "_blank"
+            );
+          }}
+        />
+        <img
+          src={GIT_ICON}
+          alt="GitHub Icon"
+          className="w-8 h-8 mt-0.5 flex hover:animate-pulse"
+          onClick={() => {
+            window.open(userData.html_url, "_blank");
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,7 @@ export const MENU_API =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0956952&lng=79.6287644&restaurantId=";
 export const MENU_IMG =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+export const GIT_ICON =
+  "https://img.icons8.com/?size=100&id=62856&format=png&color=000000";
+export const LINKEDIN_ICON =
+  "https://img.icons8.com/?size=100&id=8808&format=png&color=000000";
